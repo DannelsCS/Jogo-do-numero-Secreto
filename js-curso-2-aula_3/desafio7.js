922309247c4433df2cfaf0ab2fbccea7a2b134f8
DannelsCS <126038441+DannelsCS@users.noreply.github.com>
2024-02-13 23:31:48 -0300

    Add files via upload

---
+++ js-curso-2-aula_3/desafio7.js
@@ -0,0 +1,6 @@
+let listaGenerica = ['Daniel','Filipe','Gustavo'];
+let linguagensDeProgramação = ['JavaScript','C','C++','Kotlin','Python'];
+linguagensDeProgramação.push('Java','ruby','GoLang');
+console.log (listaGenerica [0]);
+console.log (listaGenerica [1]);
+console.log (listaGenerica [2]);
